717afdd3b868d3156f8f53c53722a519a8520efc
Jahns Michael <michael.jf.jm@gmail.com>
2024-04-24 23:09:16 +0700

    typo dataset name

--- simulate.sh
+++ simulate.sh
@@ -1,12 +1,12 @@
-# Original runs
+#!/bin/sh
 
-asreview simulate data/Jerayaman_2020_cleaned.csv -s simulation_output/nb_tf-idf.asreview --model nb --feature_extraction tfidf --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
-asreview simulate data/Jerayaman_2020_cleaned.csv -s simulation_output/svm_tf-idf.asreview --model svm --feature_extraction tfidf --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
-asreview simulate data/Jerayaman_2020_cleaned.csv -s simulation_output/lr_tf-idf.asreview --model logistic --feature_extraction tfidf --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
-asreview simulate data/Jerayaman_2020_cleaned.csv -s simulation_output/lr_doc2vec.asreview --model logistic --feature_extraction doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
-asreview simulate data/Jerayaman_2020_cleaned.csv -s simulation_output/rf_tf-idf.asreview --model rf --feature_extraction tfidf --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
-asreview simulate data/Jerayaman_2020_cleaned.csv -s simulation_output/rf_doc2vec.asreview --model rf --feature_extraction doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
-asreview simulate data/Jerayaman_2020_cleaned.csv -s simulation_output/nn-2-layer_doc2vec.asreview --model nn-2-layer --feature_extraction doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
-asreview simulate data/Jerayaman_2020_cleaned.csv -s simulation_output/power-cnn_wide-doc2vec.asreview --model power_cnn --feature_extraction wide_doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
-asreview simulate data/Jerayaman_2020_cleaned.csv -s simulation_output/lstm_doc2vec.asreview --model lstm --feature_extraction doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
-asreview simulate data/Jerayaman_2020_cleaned.csv -s simulation_output/gru_doc2vec.asreview --model gru --feature_extraction doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
+asreview simulate data/Jeyaraman_2020_cleaned.csv -s simulation_output/nb_tf-idf.asreview --model nb --feature_extraction tfidf --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
+asreview simulate data/Jeyaraman_2020_cleaned.csv -s simulation_output/svm_tf-idf.asreview --model svm --feature_extraction tfidf --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
+asreview simulate data/Jeyaraman_2020_cleaned.csv -s simulation_output/lr_tf-idf.asreview --model logistic --feature_extraction tfidf --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
+asreview simulate data/Jeyaraman_2020_cleaned.csv -s simulation_output/lr_doc2vec.asreview --model logistic --feature_extraction doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
+asreview simulate data/Jeyaraman_2020_cleaned.csv -s simulation_output/rf_tf-idf.asreview --model rf --feature_extraction tfidf --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
+asreview simulate data/Jeyaraman_2020_cleaned.csv -s simulation_output/rf_doc2vec.asreview --model rf --feature_extraction doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
+asreview simulate data/Jeyaraman_2020_cleaned.csv -s simulation_output/nn-2-layer_doc2vec.asreview --model nn-2-layer --feature_extraction doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
+asreview simulate data/Jeyaraman_2020_cleaned.csv -s simulation_output/power-cnn_wide-doc2vec.asreview --model power_cnn --feature_extraction wide_doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
+asreview simulate data/Jeyaraman_2020_cleaned.csv -s simulation_output/lstm_doc2vec.asreview --model lstm --feature_extraction doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
+asreview simulate data/Jeyaraman_2020_cleaned.csv -s simulation_output/gru_doc2vec.asreview --model gru --feature_extraction doc2vec --n_queries min --seed 200 --n_prior_included 10 --n_prior_excluded 10 --init_seed 200
